ffdd3e9f19f2a6a0f9b14453ac101aff4de62180
Alisson00 <alissonpzz889@gmail.com>
2025-11-24 00:49:47 -0300

    tradutor

--- trivia.py
+++ trivia.py
@@ -10,4 +10,9 @@ def intro():
     print("Escolha a dificuldade e tente fazer o máximo de pontos!")
     print("Boa sorte, jogador! 🚀")
     print("========================================\n")
+    
+def translate(text):
+    text = html.unescape(text)
+    url = "https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=pt-BR&q=" + text
+
    
