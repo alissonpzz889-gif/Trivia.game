c3d0f5389967cd36f5d74365339f99c93b668099
Alisson00 <alissonpzz889@gmail.com>
2025-11-24 00:48:00 -0300

    intro

--- trivia.py
+++ trivia.py
@@ -1,3 +1,9 @@
 import html
 import requests
 import random
+
+def intro():
+    print("\n========================================")
+    print("🎮 BEM-VINDO AO TRIVIA! 🎉")
+    print("========================================")
+   
